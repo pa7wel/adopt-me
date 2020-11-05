34d6d46be9ab3a81a2b02c9fc708845ba37837ea
pborkowski1 <Tromso333>
2020-11-05 20:18:58 +0100

    Add router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Pet from "./Pet";
+import { Link, Router } from "@reach/router";
 import SearchParams from "./SearchParams";
+import Details from "./Details";
 
 const App = () => {
   return (
     <React.StrictMode>
       <div>
-        <h1 id="something-important">Adopt Me!</h1>
-        <SearchParams />
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        <Router>
+          <SearchParams path="/" />
+          <Details path="/details/:id" />
+        </Router>
       </div>
     </React.StrictMode>
   );
